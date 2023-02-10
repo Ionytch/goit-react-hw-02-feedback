d390ea87a90ad808e720cf0e31876043dcac9f8c
Aleks Ionin <starik.ionytch@gmail.com>
2023-02-10 23:39:50 +0200

    component files updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,113 @@
-// import { FeedbackPage } from "./feedback/Feedback";
+import { Component } from "react";
 import { StatPage } from "./statistics/Statistics";
-import FeedbackPage from "./feedback/Feedback";
+import FeedbackOptions from "./feedback/Feedback";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-        flexDirection: 'column'
-      }}
-    >
-      <FeedbackPage />
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//         flexDirection: 'column'
+//       }}
+//     >
+//       <FeedbackPage />
       
       
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
+
+class App extends Component {
+    
+      
+        
+    state = {
+            good: 0,
+            neutral: 0,   
+            bad: 0   
+  }
+  
+  HandleClickButton = e => {
+    const option = e.target.name;
+
+    if (option) {
+      this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+    }
+  };
+
+    // CountGood = () => {
+    //     console.log('good');
+    //     console.log(this);
+    //     this.setState(prevState => ({
+    //         good: prevState.good + 1
+    //     }));
+    // }
+    
+    // CountNeutral = () => {
+    //     this.setState(evt => ({
+    //         neutral: evt.neutral + 1,
+    //     }));
+    // }
+    
+    // CountBad = () => {
+    //     this.setState(prevState => ({
+    //         bad: prevState.bad + 1,
+    //     }));
+    // }
+    
+    countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedback = () => {
+    const totalFeedback = this.countTotalFeedback();
+    const goodFeedback = this.state.good;
+    let result = 0;
+
+    if (totalFeedback > 0) {
+      result = Math.ceil((goodFeedback / totalFeedback) * 100);
+    }
+
+    return `${result}%`;
+  };
+
+    render() {
+      const { good, neutral, bad } = this.state;
+      const countTotalFeedback = this.countTotalFeedback();
+      const countPositiveFeedback = this.countPositiveFeedback();
+      const HandleClickButton = this.HandleClickButton;
+      const options = Object.keys(this.state);
+       
+      return (
+        <div>
+          <section title="Please leave feedback">
+          <div>
+            
+            
+              <h2>Please leave feedback</h2>
+              <FeedbackOptions options={options} onLeaveFeedback={HandleClickButton}/>
+          </div>
+                       
+          <div>
+                {countTotalFeedback > 0 ? (
+         <StatPage
+                onGood={good}
+                onNeutral={neutral}
+                onBad={bad}
+                onTotal={countTotalFeedback}
+                onPositive={countPositiveFeedback} />) : (<p>There is no feedback</p>)}
+            
+            </div>
+          </section>
+        </div>
+        )
+    }
+}
+
+export default App
--- src/components/feedback/Feedback.js
+++ src/components/feedback/Feedback.js
@@ -1,78 +1,23 @@
-import { Component } from "react";
-import { StatPage } from "components/statistics/Statistics";
 
-// }
-class FeedbackPage extends Component {
-    
-      
-        
-    state = {
-            good: 0,
-            neutral: 0,   
-            bad: 0   
-        }
-
-    CountGood = () => {
-        console.log('good');
-        console.log(this);
-        this.setState(prevState => ({
-            good: prevState.good + 1
-        }));
-    }
-    
-    CountNeutral = () => {
-        this.setState(evt => ({
-            neutral: evt.neutral + 1,
-        }));
-    }
-    
-    CountBad = () => {
-        this.setState(prevState => ({
-            bad: prevState.bad + 1,
-        }));
-    }
-    
-    countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedback = () => {
-    const totalFeedback = this.countTotalFeedback();
-    const goodFeedback = this.state.good;
-    let result = 0;
-
-    if (totalFeedback > 0) {
-      result = Math.ceil((goodFeedback / totalFeedback) * 100);
-    }
-
-    return `${result}%`;
-  };
-
-    render() {
-        const countTotalFeedback = this.countTotalFeedback();
-        const countPositiveFeedback = this.countPositiveFeedback();
-        return (<div>
-        <div>
-        <h2>Please leave feedback</h2>
-                <button type="button" onClick={this.CountGood}>good</button>
-                <button type="button" onClick={this.CountNeutral}>neutral</button>
-                <button type="button" onClick={this.CountBad}>bad</button>
-            </div>
-            
+function FeedbackOptions({ options, onLeaveFeedback }) {
+    return (
+        <ul>
+            {
+                options.map(option => (
+               <li key={option}>
            
-            <div>
-                {countTotalFeedback > 0 ? (
-        <> <StatPage
-                onGood={this.state.good}
-                onNeutral={this.state.neutral}
-                onBad={this.state.good}
-                onTotal={countTotalFeedback}
-                onPositive={countPositiveFeedback} /></>) : (<p>There is no feedback</p>)}
-            </div>
-        </div>
-        )
-    }
-}
+                <button type="button" name={option} onClick={onLeaveFeedback}>{option}</button>
+            </li>     
+                )
 
-export default FeedbackPage
+                )
+            }
+            
+            
+        </ul>
+    )
+}
+    
+  
+
+export default FeedbackOptions;
